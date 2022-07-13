418f93e8e75427d4029cce892bdb5321631fb488
yuseongnam <dbtjd6511@gmail.com>
2022-07-13 18:22:47 +0900

    Initial Commit

---
+++ week3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  week3
+//
+//  Created by 남유성 on 2022/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ week3/week3App.swift
@@ -0,0 +1,17 @@
+//
+//  week3App.swift
+//  week3
+//
+//  Created by 남유성 on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct week3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
